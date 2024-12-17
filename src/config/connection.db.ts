9baf57f026c6ffdf1fa8e909ee56f933c51c39eb
Angeles <angeles.cahill@gmail.com>
2024-12-17 19:11:41 -0300

    agregando .env copy

--- src/config/connection.db.ts
+++ src/config/connection.db.ts
@@ -3,7 +3,8 @@ import pg from 'pg'
 
 const { Pool } = pg
 
-const connectionString = process.env.DATABASE_URL
+//const connectionString = process.env.DATABASE_URL
+const connectionString = "postgresql://postgres:mao9684@localhost:5432/codigoark"
 
 export const db = new Pool({
     allowExitOnIdle: true,
